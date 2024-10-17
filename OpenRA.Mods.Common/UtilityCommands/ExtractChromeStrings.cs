injiiiiil/888

Expose hotkeys to localisation.
Allows the Settings > Hotkeys screen to be localised, including hotkey decriptions, groups and contexts.

The hotkey names are exposed to localisation via KeycodeExts. Hotkey modifiers are similarly exposed via ModifersExts.

The Settings > Input screen has a Zoom Modifier dropdown, which shows the localised modifier name.

The --check-yaml utility command is taught to recognise all hotkey translation, so it can validate their usage.

OpenRA.Mods.Common/UtilityCommands/ExtractChromeStrings.cs
@@ -41,7 +41,7 @@ namespace OpenRA.Mods.Common.UtilityCommands
 				.Where(t => t.Name.EndsWith("Widget", StringComparison.InvariantCulture) && t.IsSubclassOf(typeof(Widget)))
 				.ToDictionary(
 					t => t.Name[..^6],
-					t => t.GetFields().Where(f => f.HasAttribute<TranslationReferenceAttribute>()).Select(f => f.Name).ToArray())
+					t => Utility.GetFields(t).Where(Utility.HasAttribute<TranslationReferenceAttribute>).Select(f => f.Name).ToArray())
 				.Where(t => t.Value.Length > 0)
 				.ToDictionary(t => t.Key, t => t.Value);
 
@@ -319,9 +319,9 @@ namespace OpenRA.Mods.Common.UtilityCommands
 				.Where(t => t.Name.EndsWith("Widget", StringComparison.InvariantCulture) && t.IsSubclassOf(typeof(Widget)))
 				.ToDictionary(
 					t => t.Name[..^6],
-					t => t
-						.GetFields()
-						.Where(f => f.Name != "Id" && f.IsPublic && f.FieldType == typeof(string) && !f.HasAttribute<TranslationReferenceAttribute>())
+					t =>
+						Utility.GetFields(t)
+						.Where(f => f.Name != "Id" && f.IsPublic && f.FieldType == typeof(string) && !Utility.HasAttribute<TranslationReferenceAttribute>(f))
 						.Distinct()
 						.Select(f => f.Name)
 						.ToList()))

OpenRA.Mods.Common/UtilityCommands/ExtractYamlStrings.cs
@@ -43,7 +43,7 @@ namespace OpenRA.Mods.Common.UtilityCommands
 				.Where(t => t.Name.EndsWith("Info", StringComparison.InvariantCulture) && t.IsSubclassOf(typeof(TraitInfo)))
 				.ToDictionary(
 					t => t.Name[..^4],
-					t => t.GetFields().Where(f => f.HasAttribute<TranslationReferenceAttribute>()).Select(f => f.Name).ToArray())
+					t => Utility.GetFields(t).Where(Utility.HasAttribute<TranslationReferenceAttribute>).Select(f => f.Name).ToArray())
 				.Where(t => t.Value.Length > 0)
 				.ToDictionary(t => t.Key, t => t.Value);
 

OpenRA.Mods.Common/Widgets/Logic/Settings/HotkeysSettingsLogic.cs
@@ -25,6 +25,9 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 		[TranslationReference("key", "context")]
 		const string DuplicateNotice = "label-duplicate-notice";
 
+		[TranslationReference]
+		const string AnyContext = "hotkey-context-any";
+
 		readonly ModData modData;
 		readonly Dictionary<string, MiniYaml> logicArgs;
 
@@ -37,8 +40,8 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 		bool isHotkeyValid;
 		bool isHotkeyDefault;
 
-		string currentContext = "Any";
-		readonly HashSet<string> contexts = new() { "Any" };
+		string currentContext = AnyContext;
+		readonly HashSet<string> contexts = new() { AnyContext };
 		readonly Dictionary<string, HashSet<string>> hotkeyGroups = new();
 		TextFieldWidget filterInput;
 
@@ -66,7 +69,8 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 			key.Id = hd.Name;
 			key.IsVisible = () => true;
 
-			key.Get<LabelWidget>("FUNCTION").GetText = () => hd.Description + ":";
+			var desc = TranslationProvider.GetString(hd.Description) + ":";
+			key.Get<LabelWidget>("FUNCTION").GetText = () => desc;
 
 			var remapButton = key.Get<ButtonWidget>("HOTKEY");
 			WidgetUtils.TruncateButtonToTooltip(remapButton, modData.Hotkeys[hd.Name].GetValue().DisplayString());
@@ -192,7 +196,8 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 					continue;
 
 				var header = headerTemplate.Clone();
-				header.Get<LabelWidget>("LABEL").GetText = () => hg.Key;
+				var groupName = TranslationProvider.GetString(hg.Key);
+				header.Get<LabelWidget>("LABEL").GetText = () => groupName;
 				hotkeyList.AddChild(header);
 
 				var added = new HashSet<HotkeyDefinition>();
@@ -220,7 +225,8 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 		void InitHotkeyRemapDialog(Widget panel)
 		{
 			var label = panel.Get<LabelWidget>("HOTKEY_LABEL");
-			var labelText = new CachedTransform<HotkeyDefinition, string>(hd => hd?.Description + ":");
+			var labelText = new CachedTransform<HotkeyDefinition, string>(
+				hd => (hd != null ? TranslationProvider.GetString(hd.Description) : "") + ":");
 			label.IsVisible = () => selectedHotkeyDefinition != null;
 			label.GetText = () => labelText.Update(selectedHotkeyDefinition);
 
@@ -228,10 +234,13 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 			duplicateNotice.TextColor = ChromeMetrics.Get<Color>("NoticeErrorColor");
 			duplicateNotice.IsVisible = () => !isHotkeyValid;
 			var duplicateNoticeText = new CachedTransform<HotkeyDefinition, string>(hd =>
-				hd != null ?
-				TranslationProvider.GetString(DuplicateNotice, Translation.Arguments("key", hd.Description,
-					"context", hd.Contexts.First(c => selectedHotkeyDefinition.Contexts.Contains(c)))) :
-				"");
+				hd != null
+					? TranslationProvider.GetString(
+						DuplicateNotice,
+						Translation.Arguments(
+							"key", TranslationProvider.GetString(hd.Description),
+							"context", TranslationProvider.GetString(hd.Contexts.First(c => selectedHotkeyDefinition.Contexts.Contains(c)))))
+					: "");
 			duplicateNotice.GetText = () => duplicateNoticeText.Update(duplicateHotkeyDefinition);
 
 			var originalNotice = panel.Get<LabelWidget>("ORIGINAL_NOTICE");
@@ -328,8 +337,8 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 		{
 			var filter = filterInput.Text;
 			var isFilteredByName = string.IsNullOrWhiteSpace(filter) ||
-				hd.Description.Contains(filter, StringComparison.CurrentCultureIgnoreCase);
-			var isFilteredByContext = currentContext == "Any" || hd.Contexts.Contains(currentContext);
+				TranslationProvider.GetString(hd.Description).Contains(filter, StringComparison.CurrentCultureIgnoreCase);
+			var isFilteredByContext = currentContext == AnyContext || hd.Contexts.Contains(currentContext);
 
 			return isFilteredByName && isFilteredByContext;
 		}
@@ -357,9 +366,9 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 		static string GetContextDisplayName(string context)
 		{
 			if (string.IsNullOrEmpty(context))
-				return "Any";
+				context = AnyContext;
 
-			return context;
+			return TranslationProvider.GetString(context);
 		}
 	}
 }

DoGyAUT/crystallized-nexus-engine

OpenRA.Mods.Common/UtilityCommands/ExtractChromeStrings.cs
@@ -40,7 +40,7 @@ namespace OpenRA.Mods.Common.UtilityCommands
 				.Where(t => t.Name.EndsWith("Widget", StringComparison.InvariantCulture) && t.IsSubclassOf(typeof(Widget)))
 				.ToDictionary(
 					t => t.Name[..^6],
-					t => t.GetFields().Where(f => f.HasAttribute<FluentReferenceAttribute>()).Select(f => f.Name).ToArray())
+					t => Utility.GetFields(t).Where(Utility.HasAttribute<FluentReferenceAttribute>).Select(f => f.Name).ToArray())
 				.Where(t => t.Value.Length > 0)
 				.ToDictionary(t => t.Key, t => t.Value);
 

OpenRA.Mods.Common/UtilityCommands/ExtractYamlStrings.cs
@@ -43,7 +43,7 @@ namespace OpenRA.Mods.Common.UtilityCommands
 				.Where(t => t.Name.EndsWith("Info", StringComparison.InvariantCulture) && t.IsSubclassOf(typeof(TraitInfo)))
 				.ToDictionary(
 					t => t.Name[..^4],
-					t => t.GetFields().Where(f => f.HasAttribute<FluentReferenceAttribute>()).Select(f => f.Name).ToArray())
+					t => Utility.GetFields(t).Where(Utility.HasAttribute<FluentReferenceAttribute>).Select(f => f.Name).ToArray())
 				.Where(t => t.Value.Length > 0)
 				.ToDictionary(t => t.Key, t => t.Value);
 

OpenRA.Mods.Common/Widgets/Logic/Settings/HotkeysSettingsLogic.cs
@@ -25,6 +25,9 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 		[FluentReference("key", "context")]
 		const string DuplicateNotice = "label-duplicate-notice";
 
+		[FluentReference]
+		const string AnyContext = "hotkey-context-any";
+
 		readonly ModData modData;
 		readonly Dictionary<string, MiniYaml> logicArgs;
 
@@ -37,8 +40,8 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 		bool isHotkeyValid;
 		bool isHotkeyDefault;
 
-		string currentContext = "Any";
-		readonly HashSet<string> contexts = new() { "Any" };
+		string currentContext = AnyContext;
+		readonly HashSet<string> contexts = new() { AnyContext };
 		readonly Dictionary<string, HashSet<string>> hotkeyGroups = new();
 		TextFieldWidget filterInput;
 
@@ -66,7 +69,8 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 			key.Id = hd.Name;
 			key.IsVisible = () => true;
 
-			key.Get<LabelWidget>("FUNCTION").GetText = () => hd.Description + ":";
+			var desc = FluentProvider.GetString(hd.Description) + ":";
+			key.Get<LabelWidget>("FUNCTION").GetText = () => desc;
 
 			var remapButton = key.Get<ButtonWidget>("HOTKEY");
 			WidgetUtils.TruncateButtonToTooltip(remapButton, modData.Hotkeys[hd.Name].GetValue().DisplayString());
@@ -192,7 +196,8 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 					continue;
 
 				var header = headerTemplate.Clone();
-				header.Get<LabelWidget>("LABEL").GetText = () => hg.Key;
+				var groupName = FluentProvider.GetString(hg.Key);
+				header.Get<LabelWidget>("LABEL").GetText = () => groupName;
 				hotkeyList.AddChild(header);
 
 				var added = new HashSet<HotkeyDefinition>();
@@ -220,7 +225,8 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 		void InitHotkeyRemapDialog(Widget panel)
 		{
 			var label = panel.Get<LabelWidget>("HOTKEY_LABEL");
-			var labelText = new CachedTransform<HotkeyDefinition, string>(hd => hd?.Description + ":");
+			var labelText = new CachedTransform<HotkeyDefinition, string>(
+				hd => (hd != null ? FluentProvider.GetString(hd.Description) : "") + ":");
 			label.IsVisible = () => selectedHotkeyDefinition != null;
 			label.GetText = () => labelText.Update(selectedHotkeyDefinition);
 
@@ -228,10 +234,12 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 			duplicateNotice.TextColor = ChromeMetrics.Get<Color>("NoticeErrorColor");
 			duplicateNotice.IsVisible = () => !isHotkeyValid;
 			var duplicateNoticeText = new CachedTransform<HotkeyDefinition, string>(hd =>
-				hd != null ?
-				FluentProvider.GetString(DuplicateNotice,
-					"key", hd.Description,
-					"context", hd.Contexts.First(c => selectedHotkeyDefinition.Contexts.Contains(c))) : "");
+				hd != null
+					? FluentProvider.GetString(
+						DuplicateNotice,
+						"key", FluentProvider.GetString(hd.Description),
+						"context", FluentProvider.GetString(hd.Contexts.First(c => selectedHotkeyDefinition.Contexts.Contains(c))))
+					: "");
 			duplicateNotice.GetText = () => duplicateNoticeText.Update(duplicateHotkeyDefinition);
 
 			var originalNotice = panel.Get<LabelWidget>("ORIGINAL_NOTICE");
@@ -328,8 +336,8 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 		{
 			var filter = filterInput.Text;
 			var isFilteredByName = string.IsNullOrWhiteSpace(filter) ||
-				hd.Description.Contains(filter, StringComparison.CurrentCultureIgnoreCase);
-			var isFilteredByContext = currentContext == "Any" || hd.Contexts.Contains(currentContext);
+				FluentProvider.GetString(hd.Description).Contains(filter, StringComparison.CurrentCultureIgnoreCase);
+			var isFilteredByContext = currentContext == AnyContext || hd.Contexts.Contains(currentContext);
 
 			return isFilteredByName && isFilteredByContext;
 		}
@@ -357,9 +365,9 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 		static string GetContextDisplayName(string context)
 		{
 			if (string.IsNullOrEmpty(context))
-				return "Any";
+				context = AnyContext;
 
-			return context;
+			return FluentProvider.GetString(context);
 		}
 	}
 }