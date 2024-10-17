injiiiiil/888

Use short context names in hotkey yaml, generate translation key at runtime.

OpenRA.Game/HotkeyDefinition.cs
@@ -10,11 +10,14 @@
 #endregion
 
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenRA
 {
 	public sealed class HotkeyDefinition
 	{
+		public const string ContextTranslationPrefix = "hotkey-context";
+
 		public readonly string Name;
 		public readonly Hotkey Default = Hotkey.Invalid;
 
@@ -45,7 +48,8 @@ namespace OpenRA
 				Types = FieldLoader.GetValue<HashSet<string>>("Types", typesYaml.Value);
 
 			if (nodeDict.TryGetValue("Contexts", out var contextYaml))
-				Contexts = FieldLoader.GetValue<HashSet<string>>("Contexts", contextYaml.Value);
+				Contexts = FieldLoader.GetValue<HashSet<string>>("Contexts", contextYaml.Value)
+					.Select(c => ContextTranslationPrefix + "." + c).ToHashSet();
 
 			if (nodeDict.TryGetValue("Platform", out var platformYaml))
 			{

OpenRA.Mods.Common/Widgets/Logic/Settings/HotkeysSettingsLogic.cs
@@ -26,7 +26,7 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 		const string DuplicateNotice = "label-duplicate-notice";
 
 		[TranslationReference]
-		const string AnyContext = "hotkey-context-any";
+		const string AnyContext = HotkeyDefinition.ContextTranslationPrefix + "-any";
 
 		readonly ModData modData;
 		readonly Dictionary<string, MiniYaml> logicArgs;

DoGyAUT/crystallized-nexus-engine

OpenRA.Game/HotkeyDefinition.cs
@@ -10,11 +10,14 @@
 #endregion
 
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenRA
 {
 	public sealed class HotkeyDefinition
 	{
+		public const string ContextFluentPrefix = "hotkey-context";
+
 		public readonly string Name;
 		public readonly Hotkey Default = Hotkey.Invalid;
 
@@ -45,7 +48,8 @@ namespace OpenRA
 				Types = FieldLoader.GetValue<HashSet<string>>("Types", typesYaml.Value);
 
 			if (nodeDict.TryGetValue("Contexts", out var contextYaml))
-				Contexts = FieldLoader.GetValue<HashSet<string>>("Contexts", contextYaml.Value);
+				Contexts = FieldLoader.GetValue<HashSet<string>>("Contexts", contextYaml.Value)
+					.Select(c => ContextFluentPrefix + "." + c).ToHashSet();
 
 			if (nodeDict.TryGetValue("Platform", out var platformYaml))
 			{

OpenRA.Mods.Common/Widgets/Logic/Settings/HotkeysSettingsLogic.cs
@@ -26,7 +26,7 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 		const string DuplicateNotice = "label-duplicate-notice";
 
 		[FluentReference]
-		const string AnyContext = "hotkey-context-any";
+		const string AnyContext = HotkeyDefinition.ContextFluentPrefix + "-any";
 
 		readonly ModData modData;
 		readonly Dictionary<string, MiniYaml> logicArgs;