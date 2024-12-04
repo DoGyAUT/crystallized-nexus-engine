test1232156/OpenRA

Translation key fixes
Co-Authored-By: Gustas <37534529+punkpun@users.noreply.github.com>

mods/d2k/maps/atreides-01a/atreides01a.lua
@@ -54,7 +54,7 @@ AtreidesEntryPath = { AtreidesWaypoint.Location, AtreidesRally.Location }
 Messages =
 {
 	UserInterface.Translate("build-concrete"),
-	UserInterface.Translate("build-windtrap"),
+	UserInterface.Translate("build-wind-trap"),
 	UserInterface.Translate("build-refinery"),
 	UserInterface.Translate("build-silo")
 }

mods/d2k/maps/harkonnen-01a/harkonnen01a.lua
@@ -54,7 +54,7 @@ HarkonnenEntryPath = { HarkonnenWaypoint.Location, HarkonnenRally.Location }
 Messages =
 {
 	UserInterface.Translate("build-concrete"),
-	UserInterface.Translate("build-windtrap"),
+	UserInterface.Translate("build-wind-trap"),
 	UserInterface.Translate("build-refinery"),
 	UserInterface.Translate("build-silo")
 }

mods/d2k/maps/harkonnen-08/harkonnen08.lua
@@ -296,7 +296,7 @@ WorldLoaded = function()
 	end)
 
 	Trigger.OnKilledOrCaptured(OPalace, function()
-		Media.DisplayMessage(UserInterface.Translate("can-not-stand-harkonnen-must-become-neutral"), UserInterface.Translate("atreides-commander"))
+		Media.DisplayMessage(UserInterface.Translate("cannot-stand-harkonnen-must-become-neutral"), UserInterface.Translate("atreides-commander"))
 
 		ChangeOwner(AtreidesEnemy, AtreidesNeutral)
 		DefendAndRepairBase(AtreidesNeutral, AtreidesBase, 0.75, AttackGroupSize[Difficulty])

mods/d2k/maps/ordos-01a/ordos01a.lua
@@ -54,7 +54,7 @@ OrdosEntryPath = { OrdosWaypoint.Location, OrdosRally.Location }
 Messages =
 {
 	UserInterface.Translate("build-concrete"),
-	UserInterface.Translate("build-windtrap"),
+	UserInterface.Translate("build-wind-trap"),
 	UserInterface.Translate("build-refinery"),
 	UserInterface.Translate("build-silo")
 }

mods/ra/maps/allies-08a/allies08a.lua
@@ -105,7 +105,7 @@ Tick = function()
 
 	if Ticked > 0 then
 		if (Ticked % DateTime.Seconds(1)) == 0 then
-			Timer = UserInterface.Translate("chronosphere-experiment-complete-in", { ["time"] = Utils.FormatTime(Ticked) })
+			Timer = UserInterface.Translate("chronosphere-experiment-completes-in", { ["time"] = Utils.FormatTime(Ticked) })
 			UserInterface.SetMissionText(Timer, TimerColor)
 		end
 		Ticked = Ticked - 1

DoGyAUT/crystallized-nexus-engine

mods/d2k/maps/atreides-01a/atreides01a.lua
@@ -54,7 +54,7 @@ AtreidesEntryPath = { AtreidesWaypoint.Location, AtreidesRally.Location }
 Messages =
 {
 	UserInterface.GetFluentMessage("build-concrete"),
-	UserInterface.GetFluentMessage("build-windtrap"),
+	UserInterface.GetFluentMessage("build-wind-trap"),
 	UserInterface.GetFluentMessage("build-refinery"),
 	UserInterface.GetFluentMessage("build-silo")
 }

mods/d2k/maps/harkonnen-01a/harkonnen01a.lua
@@ -54,7 +54,7 @@ HarkonnenEntryPath = { HarkonnenWaypoint.Location, HarkonnenRally.Location }
 Messages =
 {
 	UserInterface.GetFluentMessage("build-concrete"),
-	UserInterface.GetFluentMessage("build-windtrap"),
+	UserInterface.GetFluentMessage("build-wind-trap"),
 	UserInterface.GetFluentMessage("build-refinery"),
 	UserInterface.GetFluentMessage("build-silo")
 }

mods/d2k/maps/harkonnen-08/harkonnen08.lua
@@ -296,7 +296,7 @@ WorldLoaded = function()
 	end)
 
 	Trigger.OnKilledOrCaptured(OPalace, function()
-		Media.DisplayMessage(UserInterface.GetFluentMessage("can-not-stand-harkonnen-must-become-neutral"), UserInterface.GetFluentMessage("atreides-commander"))
+		Media.DisplayMessage(UserInterface.GetFluentMessage("cannot-stand-harkonnen-must-become-neutral"), UserInterface.GetFluentMessage("atreides-commander"))
 
 		ChangeOwner(AtreidesEnemy, AtreidesNeutral)
 		DefendAndRepairBase(AtreidesNeutral, AtreidesBase, 0.75, AttackGroupSize[Difficulty])

mods/d2k/maps/ordos-01a/ordos01a.lua
@@ -54,7 +54,7 @@ OrdosEntryPath = { OrdosWaypoint.Location, OrdosRally.Location }
 Messages =
 {
 	UserInterface.GetFluentMessage("build-concrete"),
-	UserInterface.GetFluentMessage("build-windtrap"),
+	UserInterface.GetFluentMessage("build-wind-trap"),
 	UserInterface.GetFluentMessage("build-refinery"),
 	UserInterface.GetFluentMessage("build-silo")
 }

mods/ra/maps/allies-08a/allies08a.lua
@@ -105,7 +105,7 @@ Tick = function()
 
 	if Ticked > 0 then
 		if (Ticked % DateTime.Seconds(1)) == 0 then
-			Timer = UserInterface.GetFluentMessage("chronosphere-experiment-complete-in", { ["time"] = Utils.FormatTime(Ticked) })
+			Timer = UserInterface.GetFluentMessage("chronosphere-experiment-completes-in", { ["time"] = Utils.FormatTime(Ticked) })
 			UserInterface.SetMissionText(Timer, TimerColor)
 		end
 		Ticked = Ticked - 1