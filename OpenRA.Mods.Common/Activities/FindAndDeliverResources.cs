thumperward/OpenRA

commit 97878cecf269cc43146db54e2ac7f937e5d00145

Abstract docking logic from Harvester and Refinery

OpenRA.Mods.Common/Activities/FindAndDeliverResources.cs
@@ -24,8 +24,6 @@ namespace OpenRA.Mods.Common.Activities
 		readonly HarvesterInfo harvInfo;
 		readonly Mobile mobile;
 		readonly ResourceClaimLayer claimLayer;
-
-		Actor deliverActor;
 		CPos? orderLocation;
 		CPos? lastHarvestedCell;
 		bool hasDeliveredLoad;
@@ -34,19 +32,14 @@ namespace OpenRA.Mods.Common.Activities
 
 		public bool LastSearchFailed { get; private set; }
 
-		public FindAndDeliverResources(Actor self, Actor deliverActor = null)
+		public FindAndDeliverResources(Actor self, CPos? orderLocation = null)
 		{
 			harv = self.Trait<Harvester>();
 			harvInfo = self.Info.TraitInfo<HarvesterInfo>();
 			mobile = self.Trait<Mobile>();
 			claimLayer = self.World.WorldActor.Trait<ResourceClaimLayer>();
-			this.deliverActor = deliverActor;
-		}
-
-		public FindAndDeliverResources(Actor self, CPos orderLocation)
-			: this(self, null)
-		{
-			this.orderLocation = orderLocation;
+			if (orderLocation.HasValue)
+				this.orderLocation = orderLocation.Value;
 		}
 
 		protected override void OnFirstRun(Actor self)
@@ -62,14 +55,6 @@ namespace OpenRA.Mods.Common.Activities
 				// so we keep deliveredLoad false.
 				if (harv.IsFull)
 					QueueChild(new MoveToDock(self));
-			}
-
-			// If an explicit "deliver" order is given, the harvester goes immediately to the refinery.
-			if (deliverActor != null)
-			{
-				QueueChild(new MoveToDock(self, deliverActor));
-				hasDeliveredLoad = true;
-				deliverActor = null;
 			}
 		}
 
@@ -92,9 +77,12 @@ namespace OpenRA.Mods.Common.Activities
 			// Are we full or have nothing more to gather? Deliver resources.
 			if (harv.IsFull || (!harv.IsEmpty && LastSearchFailed))
 			{
+				// If we are reserved it means docking was already initiated and we should wait.
+				if (harv.DockClientManager.ReservedHost != null)
+					return false;
+
 				QueueChild(new MoveToDock(self));
 				hasDeliveredLoad = true;
-				return false;
 			}
 
 			// After a failed search, wait and sit still for a bit before searching again.
@@ -128,13 +116,13 @@ namespace OpenRA.Mods.Common.Activities
 			// of the refinery entrance.
 			if (LastSearchFailed)
 			{
-				var lastproc = harv.LastLinkedProc ?? harv.LinkedProc;
-				if (lastproc != null && !lastproc.Disposed)
+				var lastproc = harv.DockClientManager.LastReservedHost;
+				if (lastproc != null)
 				{
-					var deliveryLoc = lastproc.Trait<IAcceptResources>().DeliveryPosition;
-					if (self.CenterPosition == deliveryLoc && harv.IsEmpty)
+					var deliveryLoc = self.World.Map.CellContaining(lastproc.DockPosition);
+					if (self.Location == deliveryLoc && harv.IsEmpty)
 					{
-						var unblockCell = self.World.Map.CellContaining(deliveryLoc) + harv.Info.UnblockCell;
+						var unblockCell = deliveryLoc + harv.Info.UnblockCell;
 						var moveTo = mobile.NearestMoveableCell(unblockCell, 1, 5);
 						QueueChild(mobile.MoveTo(moveTo, 1));
 					}
@@ -171,14 +159,31 @@ namespace OpenRA.Mods.Common.Activities
 			}
 
 			// Determine where to search from and how far to search:
-			var procLoc = GetSearchFromProcLocation();
-			var searchFromLoc = lastHarvestedCell ?? procLoc ?? self.Location;
-			var searchRadius = lastHarvestedCell.HasValue ? harvInfo.SearchFromHarvesterRadius : harvInfo.SearchFromProcRadius;
+			// Prioritise search by these locations in this order: lastHarvestedCell -> lastLinkedDock -> self.
+			CPos searchFromLoc;
+			int searchRadius;
+			WPos? dockPos = null;
+			if (lastHarvestedCell.HasValue)
+			{
+				searchRadius = harvInfo.SearchFromHarvesterRadius;
+				searchFromLoc = lastHarvestedCell.Value;
+			}
+			else
+			{
+				searchRadius = harvInfo.SearchFromProcRadius;
+				var dock = harv.DockClientManager.LastReservedHost;
+				if (dock != null)
+				{
+					dockPos = dock.DockPosition;
+					searchFromLoc = self.World.Map.CellContaining(dockPos.Value);
+				}
+				else
+					searchFromLoc = self.Location;
+			}
 
 			var searchRadiusSquared = searchRadius * searchRadius;
 
 			var map = self.World.Map;
-			var procPos = procLoc.HasValue ? (WPos?)map.CenterOfCell(procLoc.Value) : null;
 			var harvPos = self.CenterPosition;
 
 			// Find any harvestable resources:
@@ -196,19 +201,19 @@ namespace OpenRA.Mods.Common.Activities
 
 					// Add a cost modifier to harvestable cells to prefer resources that are closer to the refinery.
 					// This reduces the tendency for harvesters to move in straight lines
-					if (procPos.HasValue && harvInfo.ResourceRefineryDirectionPenalty > 0 && harv.CanHarvestCell(loc))
+					if (dockPos.HasValue && harvInfo.ResourceRefineryDirectionPenalty > 0 && harv.CanHarvestCell(loc))
 					{
 						var pos = map.CenterOfCell(loc);
 
 						// Calculate harv-cell-refinery angle (cosine rule)
-						var b = pos - procPos.Value;
+						var b = pos - dockPos.Value;
 
 						if (b != WVec.Zero)
 						{
 							var c = pos - harvPos;
 							if (c != WVec.Zero)
 							{
-								var a = harvPos - procPos.Value;
+								var a = harvPos - dockPos.Value;
 								var cosA = (int)(512 * (b.LengthSquared + c.LengthSquared - a.LengthSquared) / b.Length / c.Length);
 
 								// Cost modifier varies between 0 and ResourceRefineryDirectionPenalty
@@ -239,19 +244,12 @@ namespace OpenRA.Mods.Common.Activities
 
 			if (orderLocation != null)
 				yield return new TargetLineNode(Target.FromCell(self.World, orderLocation.Value), harvInfo.HarvestLineColor);
-			else if (deliverActor != null)
-				yield return new TargetLineNode(Target.FromActor(deliverActor), harvInfo.DeliverLineColor);
-		}
-
-		CPos? GetSearchFromProcLocation()
-		{
-			if (harv.LastLinkedProc != null && !harv.LastLinkedProc.IsDead && harv.LastLinkedProc.IsInWorld)
-				return harv.LastLinkedProc.World.Map.CellContaining(harv.LastLinkedProc.Trait<IAcceptResources>().DeliveryPosition);
-
-			if (harv.LinkedProc != null && !harv.LinkedProc.IsDead && harv.LinkedProc.IsInWorld)
-				return harv.LinkedProc.World.Map.CellContaining(harv.LinkedProc.Trait<IAcceptResources>().DeliveryPosition);
-
-			return null;
+			else
+			{
+				var manager = harv.DockClientManager;
+				if (manager.ReservedHostActor != null)
+					yield return new TargetLineNode(Target.FromActor(manager.ReservedHostActor), manager.DockLineColor);
+			}
 		}
 	}
 }

OpenRA.Mods.Common/Activities/GenericDockSequence.cs
@@ -24,12 +24,12 @@ namespace OpenRA.Mods.Common.Activities
 	{
 		protected enum DockingState { Wait, Drag, Dock, Loop, Undock, Complete }
 
-		protected readonly Actor RefineryActor;
+		protected readonly Actor DockHostActor;
+		protected readonly IDockHost DockHost;
 		protected readonly WithDockingOverlay DockHostSpriteOverlay;
-		protected readonly Harvester Harv;
+		protected readonly DockClientManager DockClient;
 		protected readonly IDockClientBody DockClientBody;
 		protected readonly bool IsDragRequired;
-		protected readonly WVec DragOffset;
 		protected readonly int DragLength;
 		protected readonly WPos StartDrag;
 		protected readonly WPos EndDrag;
@@ -41,20 +41,30 @@ namespace OpenRA.Mods.Common.Activities
 
 		bool dockInitiated = false;
 
-		public GenericDockSequence(Actor self, Actor refineryActor, Refinery refinery)
+		public GenericDockSequence(Actor self, DockClientManager client, Actor hostActor, IDockHost host)
 		{
 			dockingState = DockingState.Drag;
-			RefineryActor = refineryActor;
-			DockHostSpriteOverlay = refineryActor.TraitOrDefault<WithDockingOverlay>();
-			IsDragRequired = refinery.IsDragRequired;
-			DragOffset = refinery.DragOffset;
-			DragLength = refinery.DragLength;
-			Harv = self.Trait<Harvester>();
+
+			DockClient = client;
 			DockClientBody = self.TraitOrDefault<IDockClientBody>();
-			StartDrag = self.CenterPosition;
-			EndDrag = refineryActor.CenterPosition + DragOffset;
 			notifyDockClients = self.TraitsImplementing<INotifyDockClient>().ToArray();
-			notifyDockHosts = refineryActor.TraitsImplementing<INotifyDockHost>().ToArray();
+
+			DockHost = host;
+			DockHostActor = hostActor;
+			DockHostSpriteOverlay = hostActor.TraitOrDefault<WithDockingOverlay>();
+			notifyDockHosts = hostActor.TraitsImplementing<INotifyDockHost>().ToArray();
+
+			if (host is IDockHostDrag sequence)
+			{
+				IsDragRequired = sequence.IsDragRequired;
+				DragLength = sequence.DragLength;
+				StartDrag = self.CenterPosition;
+				EndDrag = hostActor.CenterPosition + sequence.DragOffset;
+			}
+			else
+				IsDragRequired = false;
+
+			QueueChild(new Wait(host.DockWait));
 		}
 
 		public override bool Tick(Actor self)
@@ -65,8 +75,11 @@ namespace OpenRA.Mods.Common.Activities
 					return false;
 
 				case DockingState.Drag:
-					if (IsCanceling || !RefineryActor.IsInWorld || RefineryActor.IsDead || Harv.IsTraitDisabled)
+					if (IsCanceling || DockHostActor.IsDead || !DockHostActor.IsInWorld || !DockClient.CanDockAt(DockHostActor, DockHost, false, true))
+					{
+						DockClient.UnreserveHost();
 						return true;
+					}
 
 					dockingState = DockingState.Dock;
 					if (IsDragRequired)
@@ -75,10 +88,12 @@ namespace OpenRA.Mods.Common.Activities
 					return false;
 
 				case DockingState.Dock:
-					if (!IsCanceling && RefineryActor.IsInWorld && !RefineryActor.IsDead && !Harv.IsTraitDisabled)
+					if (!IsCanceling && !DockHostActor.IsDead && DockHostActor.IsInWorld && DockClient.CanDockAt(DockHostActor, DockHost, false, true))
 					{
 						dockInitiated = true;
 						PlayDockAnimations(self);
+						DockHost.OnDockStarted(DockHostActor, self, DockClient);
+						DockClient.OnDockStarted(self, DockHostActor, DockHost);
 						NotifyDocked(self);
 					}
 					else
@@ -87,7 +102,7 @@ namespace OpenRA.Mods.Common.Activities
 					return false;
 
 				case DockingState.Loop:
-					if (IsCanceling || !RefineryActor.IsInWorld || RefineryActor.IsDead || Harv.IsTraitDisabled || Harv.TickUnload(self, RefineryActor))
+					if (IsCanceling || DockHostActor.IsDead || !DockHostActor.IsInWorld || DockClient.OnDockTick(self, DockHostActor, DockHost))
 						dockingState = DockingState.Undock;
 
 					return false;
@@ -101,8 +116,8 @@ namespace OpenRA.Mods.Common.Activities
 					return false;
 
 				case DockingState.Complete:
-					Harv.LastLinkedProc = Harv.LinkedProc;
-					Harv.LinkProc(null);
+					DockHost.OnDockCompleted(DockHostActor, self, DockClient);
+					DockClient.OnDockCompleted(self, DockHostActor, DockHost);
 					NotifyUndocked(self);
 					if (IsDragRequired)
 						QueueChild(new Drag(self, EndDrag, StartDrag, DragLength));
@@ -145,7 +160,7 @@ namespace OpenRA.Mods.Common.Activities
 
 		public virtual void PlayUndockAnimations(Actor self)
 		{
-			if (RefineryActor.IsInWorld && !RefineryActor.IsDead && DockHostSpriteOverlay != null && !DockHostSpriteOverlay.Visible)
+			if (DockHostActor.IsInWorld && !DockHostActor.IsDead && DockHostSpriteOverlay != null && !DockHostSpriteOverlay.Visible)
 			{
 				dockingState = DockingState.Wait;
 				DockHostSpriteOverlay.Visible = true;
@@ -176,30 +191,30 @@ namespace OpenRA.Mods.Common.Activities
 		void NotifyDocked(Actor self)
 		{
 			foreach (var nd in notifyDockClients)
-				nd.Docked(self, RefineryActor);
+				nd.Docked(self, DockHostActor);
 
 			foreach (var nd in notifyDockHosts)
-				nd.Docked(RefineryActor, self);
+				nd.Docked(DockHostActor, self);
 		}
 
 		void NotifyUndocked(Actor self)
 		{
 			foreach (var nd in notifyDockClients)
-				nd.Undocked(self, RefineryActor);
+				nd.Undocked(self, DockHostActor);
 
-			if (RefineryActor.IsInWorld && !RefineryActor.IsDead)
+			if (DockHostActor.IsInWorld && !DockHostActor.IsDead)
 				foreach (var nd in notifyDockHosts)
-					nd.Undocked(RefineryActor, self);
+					nd.Undocked(DockHostActor, self);
 		}
 
 		public override IEnumerable<Target> GetTargets(Actor self)
 		{
-			yield return Target.FromActor(RefineryActor);
+			yield return Target.FromActor(DockHostActor);
 		}
 
 		public override IEnumerable<TargetLineNode> TargetLineNodes(Actor self)
 		{
-			yield return new TargetLineNode(Target.FromActor(RefineryActor), Color.Green);
+			yield return new TargetLineNode(Target.FromActor(DockHostActor), Color.Green);
 		}
 	}
 }

OpenRA.Mods.Common/Activities/MoveToDock.cs
@@ -19,66 +19,71 @@ namespace OpenRA.Mods.Common.Activities
 {
 	public class MoveToDock : Activity
 	{
-		readonly IMove movement;
-		readonly Harvester harv;
-		readonly Actor targetActor;
+		readonly DockClientManager dockClient;
+		Actor dockHostActor;
+		IDockHost dockHost;
 		readonly INotifyHarvesterAction[] notifyHarvesterActions;
 
-		Actor proc;
-
-		public MoveToDock(Actor self, Actor targetActor = null)
+		public MoveToDock(Actor self, Actor dockHostActor = null, IDockHost dockHost = null)
 		{
-			movement = self.Trait<IMove>();
-			harv = self.Trait<Harvester>();
-			this.targetActor = targetActor;
+			dockClient = self.Trait<DockClientManager>();
+			this.dockHostActor = dockHostActor;
+			this.dockHost = dockHost;
 			notifyHarvesterActions = self.TraitsImplementing<INotifyHarvesterAction>().ToArray();
-		}
-
-		protected override void OnFirstRun(Actor self)
-		{
-			if (targetActor != null && targetActor.IsInWorld)
-				harv.LinkProc(targetActor);
 		}
 
 		public override bool Tick(Actor self)
 		{
-			if (harv.IsTraitDisabled)
-				Cancel(self, true);
-
 			if (IsCanceling)
 				return true;
 
-			// Find the nearest best refinery if not explicitly ordered to a specific refinery:
-			if (harv.LinkedProc == null || !harv.LinkedProc.IsInWorld)
-				harv.ChooseNewProc(self, null);
-
-			// No refineries exist; check again after delay defined in Harvester.
-			if (harv.LinkedProc == null)
+			if (dockClient.IsTraitDisabled)
 			{
-				QueueChild(new Wait(harv.Info.SearchForDeliveryBuildingDelay));
-				return false;
+				Cancel(self, true);
+				return true;
 			}
 
-			proc = harv.LinkedProc;
-			var iao = proc.Trait<IAcceptResources>();
-
-			if (self.CenterPosition != iao.DeliveryPosition)
+			// Find the nearest DockHost if not explicitly ordered to a specific dock.
+			if (dockHost == null || !dockHost.IsEnabledAndInWorld)
 			{
-				foreach (var n in notifyHarvesterActions)
-					n.MovingToRefinery(self, proc);
-
-				var target = Target.FromActor(proc);
-				QueueChild(movement.MoveOntoTarget(self, target, iao.DeliveryPosition - proc.CenterPosition, iao.DeliveryAngle));
-				return false;
+				var host = dockClient.ClosestDock(null);
+				if (host.HasValue)
+				{
+					dockHost = host.Value.Trait;
+					dockHostActor = host.Value.Actor;
+				}
+				else
+				{
+					// No docks exist; check again after delay defined in dockClient.
+					QueueChild(new Wait(dockClient.Info.SearchForDockDelay));
+					return false;
+				}
 			}
 
-			QueueChild(new Wait(10));
-			iao.OnDock(self, this);
-			return true;
+			if (dockClient.ReserveHost(dockHostActor, dockHost))
+			{
+				if (dockHost.QueueMoveActivity(this, dockHostActor, self, dockClient))
+				{
+					foreach (var n in notifyHarvesterActions)
+						n.MovingToRefinery(self, dockHostActor);
+
+					return false;
+				}
+
+				dockHost.QueueDockActivity(this, dockHostActor, self, dockClient);
+				return true;
+			}
+			else
+			{
+				// The dock explicitely chosen by the user is currently occupied. Wait and check again.
+				QueueChild(new Wait(dockClient.Info.SearchForDockDelay));
+				return false;
+			}
 		}
 
 		public override void Cancel(Actor self, bool keepQueue = false)
 		{
+			dockClient.UnreserveHost();
 			foreach (var n in notifyHarvesterActions)
 				n.MovementCancelled(self);
 
@@ -87,10 +92,13 @@ namespace OpenRA.Mods.Common.Activities
 
 		public override IEnumerable<TargetLineNode> TargetLineNodes(Actor self)
 		{
-			if (proc != null)
-				yield return new TargetLineNode(Target.FromActor(proc), harv.Info.DeliverLineColor);
+			if (dockHostActor != null)
+				yield return new TargetLineNode(Target.FromActor(dockHostActor), dockClient.DockLineColor);
 			else
-				yield return new TargetLineNode(Target.FromActor(harv.LinkedProc), harv.Info.DeliverLineColor);
+			{
+				if (dockClient.ReservedHostActor != null)
+					yield return new TargetLineNode(Target.FromActor(dockClient.ReservedHostActor), dockClient.DockLineColor);
+			}
 		}
 	}
 }

OpenRA.Mods.Common/Traits/Buildings/Refinery.cs
@@ -12,32 +12,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using OpenRA.Activities;
-using OpenRA.Mods.Common.Activities;
 using OpenRA.Mods.Common.Effects;
 using OpenRA.Mods.Common.Traits.Render;
-using OpenRA.Primitives;
 using OpenRA.Traits;
 
 namespace OpenRA.Mods.Common.Traits
 {
-	public class RefineryInfo : TraitInfo, Requires<WithSpriteBodyInfo>, IAcceptResourcesInfo
+	public class RefineryInfo : TraitInfo, Requires<WithSpriteBodyInfo>, Requires<IDockHostInfo>
 	{
-		[Desc("Actual harvester facing when docking.")]
-		public readonly WAngle DockAngle = WAngle.Zero;
-
-		[Desc("Docking cell relative to top-left cell.")]
-		public readonly CVec DockOffset = CVec.Zero;
-
-		[Desc("Does the refinery require the harvester to be dragged in?")]
-		public readonly bool IsDragRequired = false;
-
-		[Desc("Vector by which the harvester will be dragged when docking.")]
-		public readonly WVec DragOffset = WVec.Zero;
-
-		[Desc("In how many steps to perform the dragging?")]
-		public readonly int DragLength = 0;
-
 		[Desc("Store resources in silos. Adds cash directly without storing if set to false.")]
 		public readonly bool UseStorage = true;
 
@@ -50,33 +32,16 @@ namespace OpenRA.Mods.Common.Traits
 		public override object Create(ActorInitializer init) { return new Refinery(init.Self, this); }
 	}
 
-	public class Refinery : INotifyCreated, ITick, IAcceptResources, INotifySold, INotifyCapture,
-		INotifyOwnerChanged, ISync, INotifyActorDisposing
+	public class Refinery : IAcceptResources, INotifyCreated, ITick, INotifyOwnerChanged
 	{
-		readonly Actor self;
 		readonly RefineryInfo info;
 		PlayerResources playerResources;
 		IEnumerable<int> resourceValueModifiers;
 
 		int currentDisplayTick = 0;
 		int currentDisplayValue = 0;
-
-		[Sync]
-		Actor dockedHarv = null;
-
-		[Sync]
-		bool preventDock = false;
-
-		public bool AllowDocking => !preventDock;
-		public WPos DeliveryPosition => self.World.Map.CenterOfCell(self.Location + info.DockOffset);
-		public WAngle DeliveryAngle => info.DockAngle;
-		public bool IsDragRequired => info.IsDragRequired;
-		public WVec DragOffset => info.DragOffset;
-		public int DragLength => info.DragLength;
-
 		public Refinery(Actor self, RefineryInfo info)
 		{
-			this.self = self;
 			this.info = info;
 			playerResources = self.Owner.PlayerActor.Trait<PlayerResources>();
 			currentDisplayTick = info.TickRate;
@@ -87,13 +52,7 @@ namespace OpenRA.Mods.Common.Traits
 			resourceValueModifiers = self.TraitsImplementing<IResourceValueModifier>().ToArray().Select(m => m.GetResourceValueModifier());
 		}
 
-		public IEnumerable<TraitPair<Harvester>> GetLinkedHarvesters()
-		{
-			return self.World.ActorsWithTrait<Harvester>()
-				.Where(a => a.Trait.LinkedProc == self);
-		}
-
-		int IAcceptResources.AcceptResources(string resourceType, int count)
+		int IAcceptResources.AcceptResources(Actor self, string resourceType, int count)
 		{
 			if (!playerResources.Info.ResourceValues.TryGetValue(resourceType, out var resourceValue))
 				return 0;
@@ -131,17 +90,8 @@ namespace OpenRA.Mods.Common.Traits
 			return count;
 		}
 
-		void CancelDock()
-		{
-			preventDock = true;
-		}
-
 		void ITick.Tick(Actor self)
 		{
-			// Harvester was killed while unloading
-			if (dockedHarv != null && dockedHarv.IsDead)
-				dockedHarv = null;
-
 			if (info.ShowTicks && currentDisplayValue > 0 && --currentDisplayTick <= 0)
 			{
 				var temp = currentDisplayValue;
@@ -152,49 +102,9 @@ namespace OpenRA.Mods.Common.Traits
 			}
 		}
 
-		void INotifyActorDisposing.Disposing(Actor self)
-		{
-			CancelDock();
-			foreach (var harv in GetLinkedHarvesters())
-				harv.Trait.UnlinkProc(harv.Actor, self);
-		}
-
-		public void OnDock(Actor harv, MoveToDock dockOrder)
-		{
-			if (!preventDock)
-			{
-				dockOrder.QueueChild(new CallFunc(() => dockedHarv = harv, false));
-				dockOrder.QueueChild(new HarvesterDockSequence(harv, self, this));
-				dockOrder.QueueChild(new CallFunc(() => dockedHarv = null, false));
-			}
-		}
-
 		void INotifyOwnerChanged.OnOwnerChanged(Actor self, Player oldOwner, Player newOwner)
 		{
-			// Unlink any harvesters
-			foreach (var harv in GetLinkedHarvesters())
-				harv.Trait.UnlinkProc(harv.Actor, self);
-
 			playerResources = newOwner.PlayerActor.Trait<PlayerResources>();
-		}
-
-		void INotifyCapture.OnCapture(Actor self, Actor captor, Player oldOwner, Player newOwner, BitSet<CaptureType> captureTypes)
-		{
-			// Steal any docked harv too
-			if (dockedHarv != null)
-			{
-				dockedHarv.ChangeOwner(newOwner);
-
-				// Relink to this refinery
-				dockedHarv.Trait<Harvester>().LinkProc(self);
-			}
-		}
-
-		void INotifySold.Selling(Actor self) { CancelDock(); }
-		void INotifySold.Sold(Actor self)
-		{
-			foreach (var harv in GetLinkedHarvesters())
-				harv.Trait.UnlinkProc(harv.Actor, self);
 		}
 	}
 }

OpenRA.Mods.Common/Traits/CarryableHarvester.cs
@@ -36,10 +36,9 @@ namespace OpenRA.Mods.Common.Traits
 
 		void INotifyHarvesterAction.MovingToRefinery(Actor self, Actor refineryActor)
 		{
-			var iao = refineryActor.Trait<IAcceptResources>();
-			var location = self.World.Map.CellContaining(iao.DeliveryPosition);
+			var dock = refineryActor.Trait<IDockHost>();
 			foreach (var t in transports)
-				t.RequestTransport(self, location);
+				t.RequestTransport(self, self.World.Map.CellContaining(dock.DockPosition));
 		}
 
 		void INotifyHarvesterAction.MovementCancelled(Actor self)

OpenRA.Mods.Common/Traits/DockClientBase.cs
@@ -1,0 +1,54 @@
+#region Copyright & License Information
+/*
+ * Copyright (c) The OpenRA Developers and Contributors
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	public abstract class DockClientBaseInfo : ConditionalTraitInfo, IDockClientInfo, Requires<DockClientManagerInfo> { }
+
+	public abstract class DockClientBase<InfoType> : ConditionalTrait<InfoType>, IDockClient, INotifyCreated where InfoType : DockClientBaseInfo
+	{
+		readonly Actor self;
+
+		public abstract BitSet<DockType> GetDockType { get; }
+		public DockClientManager DockClientManager { get; }
+
+		protected DockClientBase(Actor self, InfoType info)
+			: base(info)
+		{
+			this.self = self;
+			DockClientManager = self.Trait<DockClientManager>();
+		}
+
+		protected virtual bool CanDock()
+		{
+			return true;
+		}
+
+		public virtual bool IsDockingPossible(BitSet<DockType> type, bool forceEnter = false)
+		{
+			return !IsTraitDisabled && GetDockType.Overlaps(type) && (forceEnter || CanDock());
+		}
+
+		public virtual bool CanDockAt(Actor hostActor, IDockHost host, bool forceEnter = false, bool ignoreOccupancy = false)
+		{
+			return (forceEnter || self.Owner.IsAlliedWith(hostActor.Owner)) && IsDockingPossible(host.GetDockType, forceEnter) && host.IsDockingPossible(self, this, ignoreOccupancy);
+		}
+
+		public virtual void OnDockStarted(Actor self, Actor hostActor, IDockHost host) { }
+
+		public virtual bool OnDockTick(Actor self, Actor hostActor, IDockHost host) { return false; }
+
+		public virtual void OnDockCompleted(Actor self, Actor hostActor, IDockHost host) { }
+	}
+}

DoGyAUT/crystallized-nexus-engine

commit 080ec8c14a91acc626dc4f1a3e64782774046f46

Harvesters prefer ore near to the refinery.
When searching for ore, harvesters will prefer ore close to their refinery, the dockPos. However this only works when this is set, and is was not being set when expected. Fixing this prevents harvesters moving in straight lines, particularly when the ore is to the left of the refinery, which is the default path search direction.

Fixes a regression from 97878cecf269cc43146db54e2ac7f937e5d00145.

OpenRA.Mods.Common/Activities/FindAndDeliverResources.cs
@@ -173,7 +173,8 @@ namespace OpenRA.Mods.Common.Activities
 			// Prioritise search by these locations in this order: lastHarvestedCell -> lastLinkedDock -> self.
 			CPos searchFromLoc;
 			int searchRadius;
-			WPos? dockPos = null;
+			var dockPos = harv.DockClientManager?.LastReservedHost?.DockPosition;
+
 			if (lastHarvestedCell.HasValue)
 			{
 				searchRadius = harvInfo.SearchFromHarvesterRadius;
@@ -182,12 +183,8 @@ namespace OpenRA.Mods.Common.Activities
 			else
 			{
 				searchRadius = harvInfo.SearchFromProcRadius;
-				var dock = harv.DockClientManager?.LastReservedHost;
-				if (dock != null)
-				{
-					dockPos = dock.DockPosition;
+				if (dockPos != null)
 					searchFromLoc = self.World.Map.CellContaining(dockPos.Value);
-				}
 				else
 					searchFromLoc = self.Location;
 			}